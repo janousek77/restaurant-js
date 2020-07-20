4967d1aca28ee6e7f9257cddf7b64ded6ec76c9f
weann2020 <anw2@unlv.nevada.edu>
2020-07-20 15:36:32 -0700

    day 1 final version

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ document.getElementById("submit_btn").addEventListener("click", (e) => {
   e.preventDefault();
 
   //extract keywords input from the search bar
-  let keywords = document.getElementById("search-input-restaurants").value;
+  let keywords = document.getElementById("input-city").value;
   console.log(keywords); // check input
 
   fetch(`http://localhost:3000/location/${keywords}`)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((obj) => {
+      for (let i = 0; i < 10; i++) {
+        document
+          .getElementById("card-container")
+          .append(createCards(obj.data[i]));
+      }
 
-  document.getElementById("card-container").append(createCards(keywords));
+      //clear the search bar
+      document.getElementById("input-city").value = "";
+
+      console.log(obj);
+    });
 });
 
 //function to create cards
@@ -22,27 +31,28 @@ function createCards(input) {
   let img = document.createElement("img");
   img.setAttribute("class", "card-img-top");
   img.setAttribute("style", "height:fit-content");
-  img.setAttribute(
-    "src",
-    "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-  );
-  img.setAttribute("alt", "??????");
+  // img.setAttribute("src", input.photo.images.small.url);
+  // img.setAttribute("alt");
 
   let cardBody = document.createElement("div");
   cardBody.setAttribute("class", "card-body");
 
-  let cardTitle = document.createElement("h5");
+  let cardTitle = document.createElement("h4");
   cardTitle.setAttribute("class", "card-title");
-  cardTitle.innerHTML = "????????The restaurant";
+  cardTitle.innerHTML = input.name;
+
+  let address = document.createElement("h6");
+  address.setAttribute("class", "card-title");
+  address.innerHTML = input.address;
 
   let descriptionPara = document.createElement("p");
   descriptionPara.setAttribute("class", "card-text");
-  descriptionPara.innerHTML = "????????Good!";
+  // descriptionPara.innerHTML = input.description;
 
   let detailsBtn = document.createElement("a");
   detailsBtn.setAttribute("class", "btn btn-primary");
   detailsBtn.innerHTML = "View details";
-  detailsBtn.setAttribute("href", "https://www.applebees.com/en");
+  detailsBtn.setAttribute("href", input.website);
 
   cardBody.append(cardTitle, descriptionPara, detailsBtn);
   card.append(img, cardBody);

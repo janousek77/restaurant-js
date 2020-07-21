9662de6bff56d007879b9bef7c812f110879aeee
weann2020 <anw2@unlv.nevada.edu>
2020-07-21 09:10:57 -0700

    day 2 first push

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.getElementById("submit_btn").addEventListener("click", (e) => {
   //prevent refreshing
   e.preventDefault();
 
+  //clear display section from last search
+  document.getElementById("card-container").innerHTML = "";
+
   //extract keywords input from the search bar
   let keywords = document.getElementById("input-city").value;
   console.log(keywords); // check input
@@ -9,7 +12,13 @@ document.getElementById("submit_btn").addEventListener("click", (e) => {
   fetch(`http://localhost:3000/location/${keywords}`)
     .then((response) => response.json())
     .then((obj) => {
-      for (let i = 0; i < 10; i++) {
+      let count = 10;
+      for (let i = 0; i < count; i++) {
+        if (Object.keys(obj.data[i]).includes("ad_position")) {
+          console.log("we got here");
+          count++;
+          continue;
+        }
         document
           .getElementById("card-container")
           .append(createCards(obj.data[i]));
@@ -17,7 +26,7 @@ document.getElementById("submit_btn").addEventListener("click", (e) => {
 
       //clear the search bar
       document.getElementById("input-city").value = "";
-
+      //check response.json in console
       console.log(obj);
     });
 });
@@ -31,8 +40,15 @@ function createCards(input) {
   let img = document.createElement("img");
   img.setAttribute("class", "card-img-top");
   img.setAttribute("style", "height:fit-content");
-  img.setAttribute("src", input.photo.images.small.url);
-  // img.setAttribute("alt", input.photo.caption);
+  if (input.photo !== undefined) {
+    img.setAttribute("src", input.photo.images.small.url);
+  } else {
+    img.setAttribute(
+      "src",
+      "https://www.questrmg.com/wp-content/uploads/2019/03/web-banner-Top-Three-Restaurant-Trends.jpg"
+    );
+  }
+  img.setAttribute("alt", input.name);
 
   let cardBody = document.createElement("div");
   cardBody.setAttribute("class", "card-body");
@@ -43,13 +59,14 @@ function createCards(input) {
 
   let address = document.createElement("h6");
   address.setAttribute("class", "card-subtitle");
-  // address.innerHTML = Object.values(input.address).join();
   address.innerHTML = input.address;
+
+  let descriptionContainer = document.createElement("div");
+  descriptionContainer.setAttribute("class", "card-text-container");
 
   let descriptionPara = document.createElement("p");
   descriptionPara.setAttribute("class", "card-text");
-  // descriptionPara.innerHTML = input.description.substring(0, 100);
-  // descriptionPara.innerHTML = input.description;
+  descriptionPara.innerHTML = input.description;
 
   let detailsBtn = document.createElement("a");
   detailsBtn.setAttribute("class", "btn btn-primary");
@@ -61,7 +78,8 @@ function createCards(input) {
     detailsBtn.setAttribute("href", input.web_url);
   }
 
-  cardBody.append(cardTitle, descriptionPara, detailsBtn);
+  descriptionContainer.append(descriptionPara);
+  cardBody.append(cardTitle, address, descriptionContainer, detailsBtn);
   card.append(img, cardBody);
 
   return card;
